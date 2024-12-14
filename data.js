6fecc4f2af4d84759d481813c5c31bf86ddcda65
ChuChu145 <bladger1@gmail.com>
2024-12-14 19:02:31 +0000

    Update data.js

--- data.js
+++ data.js
@@ -4,20 +4,20 @@ export const menuArray = [
         ingredients: ["pepperoni", "mushrom", "mozarella"],
         id: 0,
         price: 14,
-        emoji: "Images/pizza.png"
+        emoji: "pizza.png"
     },
     {
         name: "Hamburger",
         ingredients: ["beef", "cheese", "lettuce"],
         price: 12,
-        emoji: "Images/burger.png",
+        emoji: "burger.png",
         id: 1
     },
         {
         name: "Beer",
         ingredients: ["grain, hops, yeast, water"],
         price: 12,
-        emoji: "Images/beer.png",
+        emoji: "beer.png",
         id: 2
     }
 ]
